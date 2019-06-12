57b1a89b8b90e245301c03183dfb1668dbcf309d
Patrique Legault <patrique.legault@uottawa.ca>
2019-06-12 17:15:59 -0400

    Adding the outreach_gem change

--- outreach_gem/lib/outreach_gem.rb
+++ outreach_gem/lib/outreach_gem.rb
@@ -4,3 +4,6 @@ require_relative 'helpers/helpers'
 # Used to perform restful services in Ruby
 require_relative 'rest-client/restfulclient'
 require_relative 'rest-client/resttype'
+
+# Used to perform invocations to Amazon S3 in Ruby
+require_relative 'outreach-s3/ruby-upload-s3'
